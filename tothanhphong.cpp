3d88bcf660459e8a0b45a9b33ef4a2c7e5ebcbeb
funny-zega <duy.dong4125@hcmut.edu.vn>
2025-06-26 15:09:32 +0700

    eeee

--- tothanhphong.cpp
+++ tothanhphong.cpp
@@ -7,5 +7,11 @@ main() {
    int n;
    cin>>n;
    cout<<"TTP"<<n;
-   
+   cout<<"Phan dong Thanh Phong"<<endl;
+   cout<<"Toi la sinh vien lop 20T3"<<endl;
+   cout<<"Toi la sinh vien truong DHQG HN"<<endl;
+   cout<<"Toi la sinh vien nganh CNTT"<<endl;
+   cout<<"Toi la sinh vien truong DHQG HN, nganh CNTT, lop 20T3"<<endl;
+   cout<<"Toi la sinh vien truong DHQG HN, nganh CNTT, lop 20T3, ma sinh vien TTP"<<endl;
+cout<<"be hai";
 }
